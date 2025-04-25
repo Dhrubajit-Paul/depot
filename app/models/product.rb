7dadfeb6aa19a2937356255ae96c4b60c56373fa
Dhrubajit Paul <dhrubajit.paul@brainstation-23.com>
2025-04-25 16:36:13 +0600

    Add header and footer partials along with IntelliJ configuration
    Introduced `_header.html.erb` and `_footer.html.erb` partials for a responsive layout with links and social icons. Added initial `.idea` IDE configuration files and `.gitignore` entries to exclude IntelliJ-specific files.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  validates :title, length: { minimum: 3, maximum: 50, message: "Maximum 30 characters." }
+  validates :price, numericality: { greater_than: 0.01, only_integer: false}
+  validates :title, uniqueness: true
+  validates_presence_of :title, :price
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
 end
