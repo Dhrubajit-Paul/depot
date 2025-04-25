a27c0693abc4f810e659c81ee4773f56c67aaf6d
Dhrubajit Paul <dhrubajit.paul@brainstation-23.com>
2025-04-25 19:56:01 +0600

    Add cart and line item functionality to store
    Introduced models, routes, and database tables for carts and line items. Updated the product model with associations and validation to prevent deletion of products referenced by line items. Enhanced the store view with an "Add to Cart" button for products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,19 @@
 class Product < ApplicationRecord
   validates :title, length: { minimum: 3, maximum: 50, message: "Maximum 30 characters." }
-  validates :price, numericality: { greater_than: 0.01, only_integer: false}
+  validates :price, numericality: { greater_than: 0.01, only_integer: false }
   validates :title, uniqueness: true
   validates_presence_of :title, :price
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
+    message: "must be a URL for GIF, JPG or PNG image."
   }
-
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw(:abort)
+    end
+  end
 end
